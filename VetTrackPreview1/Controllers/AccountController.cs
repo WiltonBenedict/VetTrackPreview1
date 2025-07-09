1c8f93fe15b5606b9766d70eb3e3999635b9bb30
Wilton <wilton.benedict0354@uhispano.ac.cr>
2025-07-08 21:12:38 -0600

    Add project files.

---
+++ VetTrackPreview1/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VetTrackPreview1.Models;
+using VetTrackPreview1.Data;
+namespace VetTrackPreview1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalidad login attempt.");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+    }
+}
---
+++ VetTrackPreview1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VetTrackPreview1.Models;
+namespace VetTrackPreview1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
